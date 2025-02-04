6c8b8ee9c9bd012a62e167add5902f0dfb0e6347
appyon <105517525+appyon@users.noreply.github.com>
2025-02-04 12:56:48 +0800

    Update newvital.py

--- newvital.py
+++ newvital.py
@@ -235,8 +235,9 @@ def main():
         
         time.sleep(5)
         pyautogui.typewrite(['tab', 'enter'], interval = 0.25 )
-        print("Sleeping for 18 seconds...")  # デバッグ用
-        time.sleep(20)
+        
+        time.sleep(5)
+        pyautogui.typewrite(['tab', 'enter'], interval = 0.25 )
         print("Woke up from sleep")
         pyautogui.press('tab', presses=4, interval=0.25)
         pyautogui.press('enter',interval=0.50)
